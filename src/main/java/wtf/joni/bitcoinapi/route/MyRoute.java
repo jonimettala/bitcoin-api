1d329f2a2aaed37fef6563734bb2916fa3dccd65
Joni Mettälä <joni.mettala@gmail.com>
2021-11-30 19:36:35 +0200

    Init Camel project

---
+++ src/main/java/wtf/joni/bitcoinapi/route/MyRoute.java
@@ -0,0 +1,15 @@
+package wtf.joni.bitcoinapi.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:myTimer?repeatCount=1")
+                .routeId("myTimedRoute")
+                .log("oh dog");
+    }
+}
---
+++ src/test/java/wtf/joni/bitcoinapi/BitcoinapiApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.joni.bitcoinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
